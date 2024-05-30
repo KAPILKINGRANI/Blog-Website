bc3c88b0bf2acd0a70c46c303f7a8801442c1590
Kapil <kingranikapil3@gmail.com>
2024-05-30 17:42:29 +0530

    feat:Added Login Functionality

--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,29 @@ use App\Http\Controllers\TagsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'index'])->name('blogs.index');
+Route::prefix('admin')->middleware(['auth'])->group(function () {
 
-Route::resource('tags', TagsController::class)->except(['show']);
-Route::resource('categories', CategoriesController::class)->except(['show']);
+    Route::get('/dashboard', function () {
+        return view('admin-panel.dashboard');
+    })->name('dashboard');
+    
+    Route::resource('tags', TagsController::class)->except(['show']);
+    Route::resource('categories', CategoriesController::class)->except(['show']);
+
+    Route::resource('posts', PostsController::class);
+    Route::put('posts/{post}/publish-now', [PostsController::class, 'publish'])->name('posts.publish');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 
 Route::get('/categories/{category}', [FrontendController::class, 'category'])->name('blogs.category');
 Route::get('/tags/{tag}', [FrontendController::class, 'tag'])->name('blogs.tag');
 
 Route::get('/blogs/{post}', [FrontendController::class, 'show'])->name('blogs.show');
 
-Route::resource('posts', PostsController::class);
-Route::put('posts/{post}/publish-now', [PostsController::class, 'publish'])->name('posts.publish');
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
